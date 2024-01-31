7b5df42d2f690f1c63e5425d24472b8d11b06aaf
adamw <adam@warski.org>
2024-01-31 21:24:31 +0100

    WIP

---
+++ core/src/main/scala/pres/StackTraces.scala
@@ -0,0 +1,31 @@
+package pres
+
+import kyo.*
+import zio.{Console, ZIO, ZIOAppDefault}
+
+object ZioStackTraces extends ZIOAppDefault:
+  override def run: ZIO[Any, Exception, Any] =
+    def a() = ZIO.fail(new Exception("boom!"))
+    def b() = Console.printLine("In b") *> a()
+    def c() = Console.printLine("In c") *> b()
+    def d() = Console.printLine("In d") *> c()
+
+    d()
+
+object KyoStackTraces extends KyoApp:
+  def a() = Tries.fail(new Exception("boom!"))
+  def b() = Consoles.println("In b").map(_ => a())
+  def c() = Consoles.println("In c").map(_ => b())
+  def d() = Consoles.println("In d").map(_ => c())
+
+  run {
+    d()
+  }
+
+object DirectStackTraces extends App:
+  def a() = throw new Exception("boom!")
+  def b() = { println("In b"); a() }
+  def c() = { println("In c"); b() }
+  def d() = { println("In d"); c() }
+
+  d()
